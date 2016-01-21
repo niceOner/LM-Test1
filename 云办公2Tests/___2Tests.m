2f92e7b0a9857b6e921f9f7ee55ee3aedfcd754b
niki_liushuang <niki_liushuang@163.com>
2016-01-21 16:14:41 +0800

    完善云办公

---
+++ 云办公2Tests/___2Tests.m
@@ -0,0 +1,39 @@
+//
+//  ___2Tests.m
+//  云办公2Tests
+//
+//  Created by 穆英波 on 16/1/20.
+//  Copyright © 2016年 LM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___2Tests : XCTestCase
+
+@end
+
+@implementation ___2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
