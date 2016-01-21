2ccb194e819de25d2552c545817a3359799f5294
niki_liushuang <niki_liushuang@163.com>
2016-01-21 15:50:22 +0800

    wrote a read.md

---
+++ LMClasses/LMMessage/Controller/MessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageViewController.h
+//  云办公
+//
+//  Created by 穆英波 on 16/1/20.
+//  Copyright © 2016年 LM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageViewController : UIViewController
+
+@end
---
+++ LMClasses/LMMessage/Controller/MessageViewController.m
@@ -0,0 +1,37 @@
+//
+//  MessageViewController.m
+//  云办公
+//
+//  Created by 穆英波 on 16/1/20.
+//  Copyright © 2016年 LM. All rights reserved.
+//
+
+#import "MessageViewController.h"
+
+@interface MessageViewController ()
+
+@end
+
+@implementation MessageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
